e3a24151531febb4fc35946a8948bb7cb84521d1
Meli56 <bodiou.amelie@gmail.com>
2022-10-26 09:16:59 +0200

    Remplissage de la class Lettre.java

--- Java01/src/entitees/Lettre.java
+++ Java01/src/entitees/Lettre.java
@@ -1,1 +1,25 @@
-//Future class Lettre.java
+package entitees;
+
+import project01.AdressePostale;
+/**
+ * Surcharger le constructeur de la classe M�re quand il y a des param�tres
+ * et qu'il n'y a pas de constructeur par d�faut : Personne()
+ *
+ * Quand on impl�mente une interface on doit surcharger toutes les m�thodes
+ * non valoris�es de l'interface (depuis la 1.8 -> 1.9 on peut donner des
+ * comportement par d�faut dans une m�thode d'une interface Java
+ *
+ * @author chris
+ *
+ */
+public class Lettre{
+
+        public Lettre(String nom, String prenom, AdressePostale adr) {
+                super(nom, prenom, adr);
+                // TODO Auto-generated constructor stub
+                //1er commit branch  fonction1
+        }
+
+
+
+}
